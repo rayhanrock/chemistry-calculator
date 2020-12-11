8909de76d5ff32649dc841bccf43971a564ea43a
Md Rayhan B. Khan <61648017+rayhanrock@users.noreply.github.com>
2020-12-11 17:03:21 +0600

    Add files via upload

--- ChemistryCalculator/backend/History.java
+++ ChemistryCalculator/backend/History.java
@@ -1,20 +1,29 @@
 package ChemistryCalculator.backend;
 
+import javax.swing.filechooser.FileSystemView;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintWriter;
 
 public interface History {
+    default void add(String line) throws FileNotFoundException {
 
-    //if there is no file named history.txt , a new file will be created. if already available the line will be added which is passed in.
-     default void add(String line) throws FileNotFoundException {
+
+        String PATH = FileSystemView.getFileSystemView().getDefaultDirectory().getPath() + File.separator;
+        String directoryName = PATH.concat("Chemistry Calculator");
         String txtFile = "history.txt";
-        File history = new File(txtFile);
+
+        File directory = new File(directoryName);
+        if (!directory.exists()){
+            directory.mkdir();
+        }
+
+        File history = new File(directoryName,txtFile);
 
         PrintWriter writer;
         if (history.exists() && !history.isDirectory()) {
-            writer = new PrintWriter(new FileOutputStream(new File(txtFile), true));
+            writer = new PrintWriter(new FileOutputStream(new File(directoryName, txtFile), true));
         } else {
             writer = new PrintWriter(history);
         }
